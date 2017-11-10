175b8a8b6428da71ab44746a4f043a766a5825ca
Marco Schmalz <marco@schess.ch>
2017-11-10 10:22:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rechner
+
+## Aufträge
+
+1. Geb einfache und verschachtelte Rechnungen vom Aufgabenblatt der letzten Lektion ins Eingabefeld ein, und beobachte die Veränderungen des Syntax-Baumes.
+   Ändert das Einlesen der Rechnung etwas an seiner Struktur?
+
+2. Erweitere die Liste der eingebauten Funktionen um die wichtigsten Grundrechenarten, und überprüfe ob sie korrekt funktionieren.
+
+3. Erweitere die evaluate-Funktion so, dass auch verschachtelte Rechnungen ausgewertet werden können.
+
+4. (Zusatz) Zeige unterhalb des Texteldes für die Eingabe an, ob es zu viele geöffnete oder geschlossene Klammern gibt.
---
+++ app.js
@@ -0,0 +1,145 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "_" }] */
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+/* global Vue */
+
+(function() {
+    "use strict";
+
+    // ============================================= //
+    // tokenize: Eingabe in logisch Blöcke aufteilen //
+    // ============================================= //
+    function tokenize(text) {
+        return text
+        .replace(/\(/g, " ( ")
+        .replace(/\)/g, " ) ")
+        .replace(/\n/g, " ")
+        .split(" ")
+        .filter(Boolean);
+    }
+
+    // ====================================== //
+    // parse: Struktur des Programms auslesen //
+    // ====================================== //
+    function parse(tokens) {
+        if (tokens.length === 0) {
+            throw SyntaxError("Unexpected end of program while reading");
+        }
+        var token = tokens.shift();
+        if (token === "(") {
+            var list = [];
+            while (tokens[0] !== ")") {
+                list.push(parse(tokens));
+            }
+            tokens.shift();
+            return list;
+        } else if (token === ")") {
+            throw SyntaxError("Unexpected )");
+        } else {
+            var number = parseFloat(token);
+            if (isNaN(number)) {
+                return token;
+            } else {
+                return number;
+            }
+        }
+    }
+
+    // ================================ //
+    // functions: Eingebaute Funktionen //
+    // ================================ //
+    const functions = {
+        "+": function(a, b) { return a + b; },
+
+        // ************************************************* //
+        // FIXME: Baue weitere mathematische Funktionen ein! //
+        // ************************************************* //
+
+    };
+
+    // ===================================== //
+    // evaluate: Eine Rechnung 'x' auswerten //
+    // ===================================== //
+    function evaluate(x) {
+        if (typeof x === "number") {
+            return x;
+        } else if (x instanceof Array) {
+            // Das erste Element ist der Name der Funktion
+            var func_name = x[0];
+            // Finde die Funktion in der Liste der Funktionen
+            var func = functions[func_name];
+
+            var args = [];
+            for (var i=1; i < x.length; i++) {
+                args.push(evaluate(x[i]))
+            }
+
+            // ************************************************* //
+            // FIXME: Ändere die evaluate Funktion so, dass auch //
+            //        verschachtelte Rechnungen berechnet werden //
+            //        können.                                    //
+            // ************************************************* //
+
+            // Führe die Funktion mit den verbleibenden Argumenten aus
+            return func(...args);
+        } else {
+            throw new Error("Kann Rechnung nicht interpretieren: " + x)
+        }
+    }
+
+
+
+    // ======================================== //
+    // vue.js Code zur Darstellung              //
+    // ======================================== //
+    Vue.component("item", {
+        template: "#item-template",
+        props: ["model"],
+        computed: {
+            isList: function() {
+                return this.model instanceof Array;
+            },
+        },
+    });
+
+    const vm = new Vue({
+        el: "#app",
+        data: {
+            input: "",
+            tokens: [],
+            syntax_tree: [],
+            functions: functions,
+            result: undefined,
+            error: false,
+            debug: true
+        },
+        watch: {
+            // Updated alle Daten, wenn der 'input' ändert
+            input: function(val) {
+                this.ast = [];
+                this.result = undefined;
+                this.error = false;
+                try {
+                    this.tokens = tokenize(val);
+                    this.syntax_tree = parse(this.tokens.slice());
+                    console.log(this.syntax_tree);
+                    let result = evaluate(this.syntax_tree);
+                    if (result instanceof Array) {
+                        const pprint = tree => tree instanceof Array ?
+                        "(" + tree.map(pprint).join(" ") + ")" : tree;
+                        this.result = pprint(result.slice(0, -1));
+                    } else if (typeof result === "function") {
+                        this.result = "native function: " + result.name;
+                    } else {
+                        this.result = result;
+                    }
+                } catch (error) {
+                    this.error = error;
+                    console.log(error);
+                }
+            }
+        },
+    });
+
+    // Example input:
+    vm.input = `1`;
+}());
